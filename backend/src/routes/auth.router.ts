eeca02dbc2b4a4479a24c1b7b16777c2d02e16e7
Anderson <anderson.alpin@ftc.edu.br>
2025-01-11 17:27:17 -0300

    feat(auth): add route to retrieve user session

--- backend/src/routes/auth.router.ts
+++ backend/src/routes/auth.router.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import { Container } from 'typedi';
 import AuthController from '../controllers/auth.controller';
 import { asyncHandler } from '../middlewares/asyncHandler';
+import { authMiddleware } from '../middlewares/authMiddleware';
 
 const authController = Container.get(AuthController);
 const router = Router();
 
 router.post('/signup', asyncHandler(authController.signup));
 router.post('/login', asyncHandler(authController.login));
+router.get('/session', authMiddleware, asyncHandler(authController.session));
 
 export default router;
