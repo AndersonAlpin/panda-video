2000d50f938c2214b4b713274565d2c5f9865ffd
Anderson <anderson.alpin@ftc.edu.br>
2025-01-11 17:36:31 -0300

    feat(pinia): add pluguin for state persistance

--- frontend/src/plugins/index.ts
+++ frontend/src/plugins/index.ts
@@ -8,9 +8,12 @@
 import vuetify from './vuetify'
 import pinia from '../stores'
 import router from '../router'
+import piniaPersist from 'pinia-plugin-persistedstate'
 
 // Types
 import type { App } from 'vue'
+
+pinia.use(piniaPersist);
 
 export function registerPlugins (app: App) {
   app
