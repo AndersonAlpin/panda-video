99ea58baa453015164ed4b934ea28137fedff7f1
Anderson <anderson.alpin@ftc.edu.br>
2025-01-11 16:23:10 -0300

    feat: modify default layout

--- frontend/src/layouts/default.vue
+++ frontend/src/layouts/default.vue
@@ -1,7 +1,12 @@
 <template>
-  <v-main>
-    <router-view />
-  </v-main>
+  <v-layout>
+    <v-app-bar>
+      <h1>App Bar</h1>
+    </v-app-bar>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-layout>
 </template>
 
 <script lang="ts" setup>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -9,9 +9,16 @@ import { createRouter, createWebHistory } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
 import { routes } from 'vue-router/auto-routes'
 
+const customRoutes = routes.map((route) => {
+  if (route.name === '/Home') {
+    return { ...route, path: '/' }
+  }
+  return route
+});
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: setupLayouts(routes),
+  routes: setupLayouts(customRoutes),
 })
 
 // Workaround for https://github.com/vitejs/vite/issues/11804
